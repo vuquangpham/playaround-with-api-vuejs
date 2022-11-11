c29811cec4650decff3a1afdf0eb606419b16ad1
vupham <vuquangpham2909@gmail.com>
2022-11-11 14:26:57 +0700

    initial components

---
+++ src/components/survey/UserExperiences.vue
@@ -0,0 +1,41 @@
+<template>
+  <section>
+    <BaseCard>
+      <h2>Submitted Experiences</h2>
+      <div>
+        <BaseButton>Load Submitted Experiences</BaseButton>
+      </div>
+      <ul>
+        <SurveyResult
+          v-for="result in results"
+          :key="result.id"
+          :name="result.name"
+          :rating="result.rating"
+        />
+      </ul>
+    </BaseCard>
+  </section>
+</template>
+
+<script>
+import BaseCard from "../UI/BaseCard.vue";
+import BaseButton from "../UI/BaseButton.vue";
+import SurveyResult from "./SurveyResult.vue";
+
+export default {
+  props: ["results"],
+  components: {
+    SurveyResult,
+    BaseCard,
+    BaseButton,
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
